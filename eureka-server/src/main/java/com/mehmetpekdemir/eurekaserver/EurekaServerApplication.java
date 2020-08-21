ff00bd8cfb5284e25648f09c7f7a8bb0c687fdcf
Mehmet Pekdemir <mehmetpekdemir06@hotmail.com>
2020-08-22 02:55:49 +0300

    Eureka-server added .

---
+++ eureka-server/src/main/java/com/mehmetpekdemir/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package com.mehmetpekdemir.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 
+ * @author MEHMET PEKDEMIR
+ * @since 1.0
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+
+	}
+
+}

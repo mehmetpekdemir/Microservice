ab70389f0e9b910eecaf1f240bc87e0685e8eaee
Mehmet Pekdemir <mehmetpekdemir06@hotmail.com>
2020-08-31 16:01:57 +0300

    Notification Service created .

---
+++ notification-service/src/main/java/com/mehmetpekdemir/notificationservice/service/NotificationService.java
@@ -0,0 +1,29 @@
+package com.mehmetpekdemir.notificationservice.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+import com.mehmetpekdemir.commonservice.messaging.TicketNotification;
+
+/**
+ * <p>
+ * Port: localhost: 8503
+ * </p>
+ * 
+ * @author MEHMET PEKDEMIR
+ * @since 1.0
+ */
+@EnableBinding(Sink.class)
+public class NotificationService {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(NotificationService.class);
+
+	@StreamListener(Sink.INPUT)
+	public void onNotification(TicketNotification ticketNotification) {
+		LOGGER.info("Notification sending...");
+		LOGGER.info(ticketNotification.toString());
+	}
+}

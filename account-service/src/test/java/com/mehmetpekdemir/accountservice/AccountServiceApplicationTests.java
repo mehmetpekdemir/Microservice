041d658002dcba6ca9e9055dcb6a4d8e2028ad71
Mehmet Pekdemir <mehmetpekdemir06@hotmail.com>
2020-09-02 19:04:28 +0300

    Revert " Typos"
    This reverts commit f02de6acbd469b5f3e849c822de171b487d40989.

---
+++ account-service/src/test/java/com/mehmetpekdemir/accountservice/AccountServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.mehmetpekdemir.accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 
+ * @author MEHMET PEKDEMIR
+ * @since 1.0
+ */
+@SpringBootTest
+public class AccountServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
